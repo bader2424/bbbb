9e43eb9f9ab49116acbe6e8d9505f62d14e19af8
bader2424 <bader.b2001@gmail.com>
2024-05-30 10:23:32 +0300

    Add files via upload

---
+++ FAQs.js
@@ -0,0 +1,10 @@
+
+    ///FAQs///
+
+    const faqs = document.querySelectorAll(".faq");
+    
+    faqs.forEach((faq) =>{
+        faq.addEventListener("click",() => {
+            faq.classList.toggle("active");
+        });
+    });
